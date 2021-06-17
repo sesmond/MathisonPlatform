133b2779c88962cd246f1c6a163eefb1cf1ecbc6
vincent <>
2021-06-17 19:58:33 +0800

    init

---
+++ app/main.py
@@ -0,0 +1,24 @@
+"""
+程序入口：
+先把功能入口在这列出来后续再分拆
+"""
+
+"""
+1. 数据管理
+"""
+
+
+def create_dataset():
+    """
+    创建数据集
+    :return:
+    """
+    while True:
+        data_name = input("请输入数据集名称：")
+        data_type = input("请输入数据类型：")
+        print(data_name)
+        print(data_type)
+        break
+
+if __name__ == '__main__':
+    create_dataset()
